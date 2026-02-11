48f787e92e05f61784afb0fba5ecd97e40841376
Zackzto1 <zackzto@gmail.com>
2026-02-11 12:43:25 -0800

    Add greet function and log greeting message

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-console.log("Branch B version");
-console.log("Branch A version");
-console.log("Final combined version");
+function greet(name) {
+  return "Hello " + name;
+}
+
+console.log(greet("Zack"));
