06de3979ba5b81529b8d7f3ed2c3d31fc910151f
Zackzto1 <zackzto@gmail.com>
2026-02-12 18:44:25 -0800

    Update getTimeMessage to include time-based greetings
    Enhance getTimeMessage function to return greetings based on the current hour.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ function getGreeting(name) {
 }
 
 function getTimeMessage() {
-  return "Hope you're having a great day!";
+  const hour = new Date().getHours();
+
+  if (hour < 12) {
+    return "Good morning!";
+  } else if (hour < 18) {
+    return "Good afternoon!";
+  } else {
+    return "Good evening!";
+  }
 }
 
   function getMotivation() {
